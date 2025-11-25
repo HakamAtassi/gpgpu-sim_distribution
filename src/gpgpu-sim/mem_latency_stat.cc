34a86d109e266928668d714966e4bc04b77cf84c
Hakam Atassi <univ5905@oscar.ddns.eng.ox.ac.uk>
2025-11-25 16:29:50 +0000

    about to make some really fucking aggressive changes. fuck this

--- src/gpgpu-sim/mem_latency_stat.cc
+++ src/gpgpu-sim/mem_latency_stat.cc
@@ -248,7 +248,7 @@ void memory_stats_t::memlatstat_dram_access(mem_fetch *mf) {
       totalbankreads[dram_id][bank] += ceil(mf->get_data_size() / m_memory_config->dram_atom_size);
     }
 
-      //totalbankaccesses[dram_id][bank] += ceil(mf->get_data_size() / m_memory_config->dram_atom_size);
+      totalbankaccesses[dram_id][bank] += ceil(mf->get_data_size() / m_memory_config->dram_atom_size);
 
 
     mem_access_type_stats[mf->get_access_type()][dram_id][bank] +=
